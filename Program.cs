2b7e50f0e6fc7fda41d4d914bdd005cdff0dd69d
anshadvelladath <93212393+AnshadMV@users.noreply.github.com>
2025-11-05 21:24:21 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+
+
+class Day02
+{
+
+    public static void Main(string[] args)
+    {
+        Task01.CheckEvenOdd();
+        Task01.positivity();
+    }
+}
+
+class Task01
+{
+    public static void CheckEvenOdd()
+    {
+        Console.WriteLine("Enter a Number : ");
+        int num = int.Parse(Console.ReadLine());
+        if (num % 2 == 0)
+        {
+            if (num > 0)
+            {
+                Console.WriteLine("Entered number is Even & Positive ");
+            }
+            else if (num < 0) {
+                Console.WriteLine("Entered number is Even & Negative ");
+            }
+            else
+            {
+                Console.WriteLine($" Entered number is {num} . it is a neutral number that is neither positive nor negative.");
+            }
+
+        }
+        else
+        {
+            if (num > 0)
+            {
+                Console.WriteLine("Entered number is Odd & Positive ");
+            }
+            else
+            {
+                Console.WriteLine("Entered number is Odd & Negative ");
+            }
+        }
+
+
+    }
+
+    public static void positivity()
+    {
+        Console.WriteLine("Hellow ");
+    }
+
+}
+
